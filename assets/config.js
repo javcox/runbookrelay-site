8a4f434696236d865111b01bd6e74a51bbcefc90
Javen Cinocca <javencin@gmail.com>
2026-04-09 03:53:53 -0500

    Add PostHog project key

--- assets/config.js
+++ assets/config.js
@@ -4,7 +4,7 @@ window.RUNBOOK_RELAY_CONFIG = {
   calendlyUrl: "https://calendly.com/javencin/20minopsaudit",
   analytics: {
     ga4MeasurementId: "G-ZFQPBPZYZ7",
-    posthogApiKey: "",
+    posthogApiKey: "phc_rrYJ2yyNMwe4WhzRrTYmtsVxXaX8k3vsYy4yq6fLLn9N",
     posthogApiHost: "https://us.i.posthog.com",
     clarityProjectId: "w8wxphs466",
     debug: false
